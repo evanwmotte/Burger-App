3cc849d1a33963eb8ac91d0a7616d005c334a49f
Evan Motte <evanwilliammotte@gmail.com>
2020-10-15 13:38:59 -0400

    db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", FALSE)
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheeseburger", FALSE)
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain Hamburger", FALSE)
