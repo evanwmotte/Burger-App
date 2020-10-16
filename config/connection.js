e0381442e98264141cca1d52b24c95d77ebb4700
Evan Motte <evanwilliammotte@gmail.com>
2020-10-16 12:29:05 -0400

    updates

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Hol@senior2556",
+    password: '',
     database: "burgers_db"
 });
 
